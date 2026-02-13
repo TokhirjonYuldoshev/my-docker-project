6ea6aa483f6b583a1cf8ae11848712840ac8c573
TokhirjonYuldoshev <shoxrux_prince@mail.ru>
2026-02-13 03:06:06 +0300

    Add Unit Tests with pytest

--- test_app.py
+++ test_app.py
@@ -4,3 +4,4 @@ def test_message_is_correct():
     """Проверяем, что приложение возвращает правильную фразу"""
     expected = "Hello from Docker! The application is running successfully."
     assert app.get_message() == expected
+    
